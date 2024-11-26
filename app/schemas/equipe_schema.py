f215da5f60e9e43dc1d20c94a52fa6c5a38a0052
AntDeivid <deividsantos121@gmail.com>
2024-11-26 08:56:52 -0300

    correção do schema de equipe

--- app/schemas/equipe_schema.py
+++ app/schemas/equipe_schema.py
@@ -8,7 +8,7 @@ class EquipeSchema(BaseModel):
     estadio: str
     apelido: str
     jogadores_registrados: int
-    info_tabela: InfoTabelaSchema
+    info_tabela_id: int
 
     class Config:
         orm_mode = True
