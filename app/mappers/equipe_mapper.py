cefb0b01dfc9b38bc50152ae933f05edfa28089d
AntDeivid <deividsantos121@gmail.com>
2024-11-26 23:22:04 -0300

    mappers atualizados

--- app/mappers/equipe_mapper.py
+++ app/mappers/equipe_mapper.py
@@ -1,26 +1,41 @@
-from mapper.object_mapper import ObjectMapper
-
 from app.models.equipe import Equipe
+from app.schemas.EquipeCreateSchema import EquipeCreateSchema
 from app.schemas.equipe_schema import EquipeSchema
 
-mapper = ObjectMapper()
 
-toSchema = {
-    "info_tabela.id": "info_tabela_id"
-}
+class EquipeMapper:
 
-toEntity = {
-    "info_tabela_id": "info_tabela.id"
-}
+    @staticmethod
+    def to_schema(equipe: Equipe) -> EquipeSchema:
+        return EquipeSchema(
+            id=equipe.id,
+            nome=equipe.nome,
+            estadio=equipe.estadio,
+            apelido=equipe.apelido,
+            jogadores_registrados=equipe.jogadores_registrados,
+            info_tabela_id=equipe.info_tabela_id,
+        )
 
-mapper.create_map(
-    Equipe,
-    EquipeSchema,
-    toSchema
-)
+    @staticmethod
+    def to_entity(equipe_schema: EquipeSchema) -> Equipe:
+        equipe = Equipe(
+            nome=equipe_schema.nome,
+            estadio=equipe_schema.estadio,
+            apelido=equipe_schema.apelido,
+            jogadores_registrados=equipe_schema.jogadores_registrados,
+        )
 
-mapper.create_map(
-    EquipeSchema,
-    Equipe,
-    toEntity
-)
+        return equipe
+
+    @staticmethod
+    def create_to_entity(equipe_create_schema: EquipeCreateSchema, equipe_id: int, info_tabela: int) -> Equipe:
+        equipe = Equipe(
+            id=equipe_id,
+            nome=equipe_create_schema.nome,
+            estadio=equipe_create_schema.estadio,
+            apelido=equipe_create_schema.apelido,
+            jogadores_registrados=equipe_create_schema.jogadores_registrados,
+            info_tabela_id=info_tabela,
+        )
+
+        return equipe
--- app/mappers/info_tabela_mapper.py
+++ app/mappers/info_tabela_mapper.py
@@ -1,10 +1,27 @@
-from mapper.object_mapper import ObjectMapper
-
 from app.models.info_tabela import InfoTabela
 from app.schemas.info_tabela_schema import InfoTabelaSchema
 
-mapper = ObjectMapper()
 
-mapper.create_map(InfoTabela, InfoTabelaSchema)
+class InfoTabelaMapper:
 
-mapper.create_map(InfoTabelaSchema, InfoTabela)
+    @staticmethod
+    def to_schema(info_tabela: InfoTabela) -> InfoTabelaSchema:
+        return InfoTabelaSchema(
+            id=info_tabela.id,
+            pontos=info_tabela.pontos,
+            jogos=info_tabela.jogos,
+            vitorias=info_tabela.vitorias,
+            empates=info_tabela.empates,
+            derrotas=info_tabela.derrotas,
+        )
+
+    @staticmethod
+    def to_entity(info_tabela_schema: InfoTabelaSchema, info_tabela_id: int) -> InfoTabela:
+        return InfoTabela(
+            id=info_tabela_id,
+            pontos=info_tabela_schema.pontos,
+            jogos=info_tabela_schema.jogos,
+            vitorias=info_tabela_schema.vitorias,
+            empates=info_tabela_schema.empates,
+            derrotas=info_tabela_schema.derrotas,
+        )
